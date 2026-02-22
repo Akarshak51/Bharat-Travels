0d60a06f636cfee2f7135a4213bc08a3d98364b4
Akarashak Mishra <mishra46er@gmail.com>
2026-02-22 09:32:19 +0530

    Create Tour.js model file for MongoDB tour schema

---
+++ models/Tour.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const tourSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    duration: {
+        type: Number,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+module.exports = mongoose.model('Tour', tourSchema);
